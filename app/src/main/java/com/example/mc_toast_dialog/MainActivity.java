342b3c425556c4d882b6f07e528981efb1bd21e5
Muhammad Ahsan <muhammad.ahsan9776@gmail.com>
2021-11-14 23:18:51 +0500

    Tested Toast

--- app/src/main/java/com/example/mc_toast_dialog/MainActivity.java
+++ app/src/main/java/com/example/mc_toast_dialog/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.mc_toast_dialog;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    Button toastBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toastBtn=findViewById(R.id.toast);
+        toastBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.toast:
+                Intent intent = new Intent(MainActivity.this, ToastTest.class);
+                startActivity(intent);
+        }
     }
 }
--- app/src/main/java/com/example/mc_toast_dialog/ToastTest.java
+++ app/src/main/java/com/example/mc_toast_dialog/ToastTest.java
@@ -4,19 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-public class Toast extends AppCompatActivity implements View.OnClickListener{
+import android.widget.Button;
+import android.widget.Toast;
 
+public class ToastTest extends AppCompatActivity implements View.OnClickListener{
+
+    Button displayToast;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_toast);
+
+        displayToast = findViewById(R.id.displayToast);
+        displayToast.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         if(v.getId()==R.id.displayToast){
-            Toast toast= new Toast();
-            toast.setDuration();
+            Toast toast = Toast.makeText(this,"This is a toast",Toast.LENGTH_SHORT);
             toast.show();
         }
     }
